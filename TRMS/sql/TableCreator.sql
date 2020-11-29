fa92c47cf1459f859a334ea3ca84f9cdf826b7f5
Brett-Kellz94 <brett.kelley@hotmail.com>
2020-11-29 12:19:15 -0500

    Tables added

--- TRMS/sql/TableCreator.sql
+++ TRMS/sql/TableCreator.sql
@@ -3,14 +3,27 @@ employeeid int not null,
 first_name varchar(100) not null,
 last_name varchar(100) not null,
 title varchar(100) not null,
-supervisorid varchar(100) not null,
+supervisorid varchar(100),
 employee_type int not null, 
 );
+
+insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (1, 'Brendan', 'Frasier', 'Sales Associate', 2, 1);
+insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (2, 'Rita', 'Smith', 'Retail Manager', 3, 2);
+insert into employee (employeeid, first_name, last_name, title, employee_type) values (3, 'Alex', 'Murphy', 'Department Manager',  3);
+insert into employee (employeeid, first_name, last_name, title, employee_type) values (4, 'Pete', 'Michaels', 'Benefits Coordinator', 4);
+insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (5, 'Marsha', 'Davis', 'Secretary', 3, 1);
+
 
 create table login_info ( employeeid int not null,
 account_password varchar (10),
 employee_type int,
 constraint pk_employeeid primary key (employeeid));
+
+insert into login_info (employeeid, account_password, employee_type) values (1, 'password', 1);
+insert into login_info (employeeid, account_password, employee_type) values (2, 'password', 2);
+insert into login_info (employeeid, account_password, employee_type) values (3, 'password', 3);
+insert into login_info (employeeid, account_password, employee_type) values (4, 'password', 4);
+insert into login_info (employeeid, account_password, employee_type) values (5, 'password', 1);
 
 create table request (
 requestid serial not null,
@@ -29,7 +42,7 @@ constraint pk_requestid primary key (requestid));
 create table final_grade (
 gradeid serial,
 requestid int not null,
-grading_format varchar (30),
+grading_format varchar (100),
 final_grade varchar (30) not null,
 upload bytea,
 constraint pk_grade primary key (gradeid));
@@ -41,9 +54,3 @@ employeeid int not null,
 award_amount numeric (10,2),
 constraint pk_awardid primary key (awardid));
 
-insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (1, 'Brendan', 'Frasier', 'Sales Associate', 2, 1);
-insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (2, 'Rita', 'Smith', 'Retail Manager', 3, 2);
-insert into employee (employeeid, first_name, last_name, title, employee_type) values (3, 'Alex', 'Murphy', 'Department Manager',  3);
-insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (5, 'Marsha', 'Davis', 'Secretary', 3, 1);
-
-insert into login_info (employeeid, account_password, employee_type) values (5, 'password', 1);
