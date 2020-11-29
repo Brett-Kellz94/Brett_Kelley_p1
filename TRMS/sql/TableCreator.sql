f2ec7245625e23c865ea4a455eb9be5db29c9738
Brett-Kellz94 <brett.kelley@hotmail.com>
2020-11-29 14:40:47 -0500

    whfdhksd

--- TRMS/sql/TableCreator.sql
+++ TRMS/sql/TableCreator.sql
@@ -1,10 +1,11 @@
+drop table employee;
 create table employee (
 employeeid int not null,
 first_name varchar(100) not null,
 last_name varchar(100) not null,
 title varchar(100) not null,
-supervisorid varchar(100),
-employee_type int not null, 
+supervisorid int,
+employee_type int not null 
 );
 
 insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (1, 'Brendan', 'Frasier', 'Sales Associate', 2, 1);
@@ -13,7 +14,7 @@ insert into employee (employeeid, first_name, last_name, title, employee_type) v
 insert into employee (employeeid, first_name, last_name, title, employee_type) values (4, 'Pete', 'Michaels', 'Benefits Coordinator', 4);
 insert into employee (employeeid, first_name, last_name, title, supervisorid, employee_type) values (5, 'Marsha', 'Davis', 'Secretary', 3, 1);
 
-
+drop table login_info;
 create table login_info ( employeeid int not null,
 account_password varchar (10),
 employee_type int,
@@ -25,6 +26,7 @@ insert into login_info (employeeid, account_password, employee_type) values (3, 
 insert into login_info (employeeid, account_password, employee_type) values (4, 'password', 4);
 insert into login_info (employeeid, account_password, employee_type) values (5, 'password', 1);
 
+drop table request;
 create table request (
 requestid serial not null,
 employeeid int not null,
@@ -39,6 +41,7 @@ department_head_approval varchar(100),
 benco_approval varchar(100),
 constraint pk_requestid primary key (requestid));
 
+drop table final_grade;
 create table final_grade (
 gradeid serial,
 requestid int not null,
@@ -47,6 +50,7 @@ final_grade varchar (30) not null,
 upload bytea,
 constraint pk_grade primary key (gradeid));
 
+drop table award;
 create table award ( 
 awardid serial not null,
 requestid int not null,
